7b776ce6645299d3676e8a6e452cb354cae1f800
DanlinJia <jdlsx21499@gmail.com>
2019-10-18 19:46:44 -0400

    Update ex1_q3.m

--- ex1_q3.m
+++ ex1_q3.m
@@ -9,12 +9,40 @@ v = zeros(1,N);
 for i=1:N
     v(i) = normrnd(0,sigma);
 end
-y = polyval(w,x)+v
-lambda=1;
-
-for gamma=10^-2:10^2
-    gamma
+y = polyval(wTrue,x)+v;
+input = zeros(3,N);
+for order=1:4
+    a = x;
+    if order==4
+        a = ones(1,N);
+    else
+        for i=1:order
+            a = a.*x;
+        end
+    end
+    input(order,:)=a;
 end
+input = [input ]
+lambda=1;
+input;
+
+percent = zeros(4,
+for gamma=
+    result=zeros(1,100);
+    for i=1:100
+        map = @(w)(w)*(w)'/gamma^2+sum(y-w*input/sigma^2);
+        x0 = rand(1,4);
+        [w,mval] = fminunc(map,x0);
+        result(:,i) = norm(wTrue-w);
+    end
+    
+    percen = 
+    prctile(result,)
+end
+
+figure(1),
+plot
+
 
 
 
@@ -22,6 +50,6 @@ function g = evalGaussian(x,mu,Sigma)
 % Evaluates the Gaussian pdf N(mu,Sigma) at each coumn of X
 [n,N] = size(x);
 C = ((2*pi)^n * det(Sigma))^(-1/2);
-E = -0.5*sum((x-repmat(mu,4,N)).*(inv(Sigma)*(x-repmat(mu,4,N))),1);
+E = -0.5*sum((x-repmat(mu,1,N)).*(inv(Sigma)*(x-repmat(mu,1,N))),1);
 g = C*exp(E);
 end
