0c62fc19852536a7eed664f320e825896b7f3184
DanlinJia <jdlsx21499@gmail.com>
2019-10-19 17:33:15 -0400

    Update ex1_q2.m

--- ex1_q2.m
+++ ex1_q2.m
@@ -1,32 +1,45 @@
-sigma_x = 0.25; sigma_y = 0.25;
+sigma_x = 0.25; sigma_y = 0.25;sigma=0.1;
 pair = zeros(2,1);
-measurement = -1;
-map = 0;
-horizontalGrid = linspace(floor(-2),ceil(2),101);
-verticalGrid = linspace(floor(-2),ceil(2),91);
+ri = -1;
+horizontalGrid = linspace(-2,2,101);
+verticalGrid = linspace(-2,2,91);
 [h,v] = meshgrid(horizontalGrid,verticalGrid);
 hv = [h(:)';v(:)'];
 [a,b] = size(hv) ; 
-pair(1) = rand();
-pair(2) = rand();
-for i=1:4
+r=rand();
+pair(1) = r*cos(2*pi*rand());
+pair(2) = r*sin(2*pi*rand());
+%     pair(1) = cos(pi/4);
+%     pair(2) = sin(pi/4);
+for i=1:9
+    r=rand()
+    pair(1) = r*cos(2*pi*rand());
+    pair(2) = r*sin(2*pi*rand());
     reference=zeros(2,i);
     reference(:) = generateDataOnCircle(i);
-    for k=1:i   
+    sumi=0;
+    for k=1:i  
         dTi = norm(pair - reference(:,k));
-        C =zeros(1,b);
+        ni = normrnd(0,sigma);
+        while ri<0
+            ri=dTi+ni;
+        end
+        M =zeros(1,b);
         E = zeros(1,b);
         for v= 1:b
-            C(:,v) = norm(hv(:,v)-reference(:,k));  
-            E(:,v) = evalPrior(hv(:,v),sigma_x, sigma_y );
+            M(:,v) = (ri - norm(hv(:,v)-reference(:,k)))^2/sigma^2;  
         end
-        map = map + log(evalGaussian(C, dTi, 0.1)) + log(E);
+        sumi = sumi+M;
     end
+    for v=1:b
+        E(:,v) = evalPrior(hv(:,v),sigma_x, sigma_y );
+    end
+    map = sumi + E;
     minDSGV = -2;
     maxDSGV = 2;
-    mapGrid = reshape(map,91,101);
-    subplot(2,2,i), 
-    contour(horizontalGrid,verticalGrid,mapGrid); hold on,
+    mapGrid = reshape(sumi,91,101);
+    subplot(3,3,i), 
+    contour(horizontalGrid,verticalGrid,-mapGrid,30); hold on,
     plot(pair(1), pair(2), 'gx');hold on,
     plot(reference(1,:), reference(2,:), 'ro');
     legend("Contour",'TruePoint','Reference'), 
@@ -49,19 +62,17 @@ end
 k=v;
 end
 
+% function p = evalPrior(pair, sigma_x, sigma_y)
+% % Generate a location based on sigmas
+% V(:,:,1) =[sigma_x^2, 0; 0, sigma_y^2];
+% C = (2*pi*sigma_x*sigma_y)^(-1);
+% E = -0.5*pair'*inv(V)*pair;
+% p = C*exp(E);
+% end
+
 function p = evalPrior(pair, sigma_x, sigma_y)
 % Generate a location based on sigmas
 V(:,:,1) =[sigma_x^2, 0; 0, sigma_y^2];
-C = (2*pi*sigma_x*sigma_y)^(-1);
-E = -0.5*pair'*inv(V)*pair;
-p = C*exp(E);
-end
-
-
-function g = evalGaussian(x,mu,Sigma)
-% Evaluates the Gaussian pdf N(mu,Sigma) at each coumn of X
-[n,N] = size(x);
-C = ((2*pi)^n * det(Sigma))^(-1/2);
-E = -0.5*sum((x-repmat(mu,1,N)).*(inv(Sigma)*(x-repmat(mu,1,N))),1);
-g = C*exp(E);
+E = pair'*inv(V)*pair;
+p = E;
 end
